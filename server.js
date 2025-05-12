93280f5e2deaf0fcf9e52d5d36ef37e96cfc3776
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-05-12 18:21:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("🤣🤣🤣🎉🎉🎉Hello world");
+    res.send("🎉🎉🎉Hello world");
     
 })
 app.listen(3000,()=>{
